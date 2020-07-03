6660980993c431963210edc55527ea6402e9fcaa
Michel <michel@home.desktop>
2020-07-03 14:13:33 -0300

    Some more functions

--- src/dictionary.rs
+++ src/dictionary.rs
@@ -1,4 +1,5 @@
 use onig::Regex;
+use rand::RngCore;
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::error;
@@ -105,14 +106,17 @@ impl Dictionary {
             .iter()
             .enumerate()
             .map(|(i, sentence)| (i, sentence.to_lowercase()))
-            .map(|(i, sentence)| (i, split_words(&sentence)))
-            .flat_map(|(i, words)| std::iter::repeat(i).zip(words.into_iter()))
-            .for_each(|(i, word)| insert_word_into_indices(&mut indices, &word, i));
+            .for_each(|(i, sentence)| {
+                let words = split_words(&sentence);
+                for word in words {
+                    insert_word_into_indices(&mut indices, word, i);
+                }
+            });
         self.indices = indices
     }
 
-    fn knows_sentence(&self, sentence: &String) -> bool {
-        self.sentences.binary_search(&sentence).is_ok()
+    fn knows_sentence(&self, sentence: &str) -> bool {
+        self.sentences.binary_search(&sentence.to_owned()).is_ok()
     }
 
     fn knows_word(&self, word: &str) -> bool {
@@ -122,7 +126,7 @@ impl Dictionary {
     pub fn learn(&mut self, line: &str) -> bool {
         let mut learned_something = false;
         for sentence in split_sentences(&line.to_lowercase()) {
-            if self.knows_sentence(&sentence) {
+            if self.knows_sentence(sentence) {
                 continue;
             }
             self.sentences.push(sentence.to_owned());
@@ -136,26 +140,46 @@ impl Dictionary {
         }
         learned_something
     }
+
+    pub fn respond_to<R: RngCore>(&self, line: &str, rng: &mut R) -> Option<&str> {
+        /*let known_words = self.known_words(line);
+        if known_words.is_empty() {
+            None
+        } else {
+            let pivot = &known_words[rng.next_u64() as usize % known_words.len()];
+            todo!()
+        }*/
+        todo!()
+    }
+
+    fn known_words(&self, line: &str) -> Vec<String> {
+        split_words(&line.to_lowercase())
+            .iter()
+            .filter(|s| self.knows_word(s))
+            .map(|s| s.to_string())
+            .collect::<Vec<_>>()
+    }
+
+    fn sentences_with_word(&self, word: &str) -> Vec<&str> {
+        self.indices
+            .get(word)
+            .and_then(|ys| Some(ys.iter().map(|y| self.sentences[*y].as_str()).collect()))
+            .unwrap_or_else(|| vec![])
+    }
 }
 
-fn split_sentences(s: &str) -> Vec<String> {
+fn split_sentences(s: &str) -> Vec<&str> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"(?<=[.!?]+)\s+").unwrap();
     }
-    RE.split(s)
-        .filter(|s| !s.is_empty())
-        .map(|s| String::from(s))
-        .collect()
+    RE.split(s).filter(|s| !s.is_empty()).collect()
 }
 
-fn split_words(s: &str) -> Vec<String> {
+fn split_words(s: &str) -> Vec<&str> {
     lazy_static! {
         static ref RE: Regex = Regex::new(r"[,.!?:\s]+").unwrap();
     }
-    RE.split(s)
-        .filter(|s| !s.is_empty())
-        .map(|s| String::from(s))
-        .collect()
+    RE.split(s).filter(|s| !s.is_empty()).collect()
 }
 
 fn sort_sentences(sentences: &mut Vec<String>) {
@@ -435,5 +459,88 @@ mod tests {
             },
             dict
         );
+    }
+
+    #[test]
+    #[ignore]
+    fn test_respond() {/*
+        let dict = Dictionary {
+            sentences: vec![
+                "hey there everyone".to_string(),
+                "everyone is a crab".to_string(),
+                "crabs are great".to_string(),
+                "there are many crabs".to_string(),
+                "crabs".to_string(),
+            ],
+            indices: hashmap![
+                "are".to_string() => vec![1, 4],
+                "there".to_string() => vec![3, 4],
+                "great".to_string() => vec![1],
+                "everyone".to_string() => vec![2, 3],
+                "crab".to_string() => vec![2],
+                "a".to_string() => vec![2],
+                "is".to_string() => vec![2],
+                "crabs".to_string() => vec![0, 1, 4],
+                "hey".to_string() => vec![3],
+                "many".to_string() => vec![4]
+            ],
+        };
+        use rand::rngs::mock::StepRng;
+        let rng = StepRng::new(0, 2);*/
+        todo!()
+    }
+
+    #[test]
+    fn test_known_words() {
+        let dict = Dictionary{
+            sentences: vec![
+                "hello world!".to_string(),
+                "i love pizza.".to_string()
+            ],
+            indices: hashmap![
+                "hello".to_string() => vec![0],
+                "world".to_string() => vec![0],
+                "i".to_string() => vec![1],
+                "love".to_string() => vec![1],
+                "pizza".to_string() => vec![1]
+            ]
+        };
+
+        let empty: Vec<&str> = vec![];
+
+        assert_eq!(vec!["i", "love", "pizza"], dict.known_words("I Love Pizza"));
+        assert_eq!(vec!["i", "pizza"], dict.known_words("I Hate Pizza!"));
+        assert_eq!(vec!["i", "love"], dict.known_words("I Love You"));
+        assert_eq!(empty, dict.known_words("foo likes cake"));
+        assert_eq!(empty, dict.known_words("pizzacake"));
+    }
+
+    #[test]
+    fn test_sentences_with_word() {
+        let dict = Dictionary{
+            sentences: vec![
+                "hello world!".to_string(),
+                "i love pizza.".to_string(),
+                "pizza is like, cool".to_string(),
+            ],
+            indices: hashmap![
+                "hello".to_string() => vec![0],
+                "world".to_string() => vec![0],
+                "i".to_string() => vec![1],
+                "love".to_string() => vec![1],
+                "pizza".to_string() => vec![1, 2],
+                "is".to_string() => vec![2],
+                "like".to_string() => vec![2],
+                "cool".to_string() => vec![2]
+            ]
+        };
+
+        let empty: Vec<&str> = vec![];
+
+        assert_eq!(vec!["i love pizza.", "pizza is like, cool"], dict.sentences_with_word("pizza"));
+        assert_eq!(vec!["i love pizza."], dict.sentences_with_word("love"));
+        assert_eq!(empty, dict.sentences_with_word("nonexisting"));
+        assert_eq!(empty, dict.sentences_with_word("luve"));
+        assert_eq!(empty, dict.sentences_with_word(""));
     }
 }
